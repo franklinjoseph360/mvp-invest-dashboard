046215458d4fcec4dbd41a2f885b46343a8b2520
Franklin Joseph <franklinjoseph360@gmail.com>
2025-03-28 23:08:51 +0800

    feat(set-up-redis): add ioredis package and connect it with express app

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+const { connectToRedis } = require('./services/redis');
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
-});
+async function startServer() {
+    console.info('Starting express server with redis ....');
+    try {
+        const redisClient = await connectToRedis(REDIS_PORT)
+        await redisClient.connect();
+        console.info('Redis connected on PORT', REDIS_PORT)
+        app.locals.redis = redisClient
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    } catch (err) {
+        console.err(err);
+    }
+}
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+startServer();
